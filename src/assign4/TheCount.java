d922155d4474e50a7dde4033657c3fc53cb01cd3
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-12 10:16:03 -0800

    Finished the count

--- src/assign4/TheCount.java
+++ src/assign4/TheCount.java
@@ -1,12 +1,10 @@
 package assign4;
 
-import java.util.concurrent.*;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
 
 @SuppressWarnings("serial")
 public class TheCount extends JPanel{
@@ -19,11 +17,14 @@ public class TheCount extends JPanel{
 	private int goal;
 	private int current;
 	private WorkerThread wt;
+	private int defaultGoal = 100000000;
+	String name = "";
 
-	public TheCount() {
-		goal = 100000000;
+	public TheCount(int name) {
+		goal = defaultGoal;
 		current = 0;
-		wt = new WorkerThread();
+		this.name = "Thread:" + name;
+		wt = new WorkerThread(this.name);
 	}
 	
 	public void createAndShowGui(){
@@ -44,57 +45,83 @@ public class TheCount extends JPanel{
 	}
 	
 	public class WorkerThread extends Thread{
+		public WorkerThread(String name){
+			super(name);
+		}
+		
 		public void run(){
-			
+			try{
+				while(current <= goal){
+					if(current%10000 == 0){
+						//Need to do this so it shows only the increments
+						int value = current;
+						//Need to sleep and update the jlabel
+						sleep(100);
+						SwingUtilities.invokeLater(new Runnable(){
+							public void run(){
+								currentStanding.setText(value + "");
+							}
+						});
+					}
+					current = current + 1;
+				}
+			}catch(Exception e){
+				System.out.println(this.getName() + " interrupted");
+			}
 		}
 	}
 	
 	private void addListeners(){
 		goalTextField.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String text = goalTextField.getText();
-				goal = Integer.parseInt(text);
+				updateGoal();
 			}
 		});
 		goalTextField.getDocument().addDocumentListener(new DocumentListener() {
 			@Override
 			public void insertUpdate(DocumentEvent e) {
-				String text = goalTextField.getText();
-				goal = Integer.parseInt(text);
+				updateGoal();
 			}
 			@Override
 			public void removeUpdate(DocumentEvent e) {
-				String text = goalTextField.getText();
-				goal = Integer.parseInt(text);
+				updateGoal();
 			}
 			@Override
 			public void changedUpdate(DocumentEvent e) {
-				String text = goalTextField.getText();
-				goal = Integer.parseInt(text);
+				updateGoal();
 			}
 		});
 		start.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Thread started");
-				if(wt.isAlive()){
+				if(wt != null && wt.isAlive()){
 					wt.interrupt();
 					wt = null;
 				}
 				current = 0;
 				currentStanding.setText(current + "");
-				wt = new WorkerThread();
+				wt = new WorkerThread(name);
 				wt.start();
 			}
 		});
 		stop.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Thread stopped");
-				if(wt.isAlive()){
+				if(wt != null && wt.isAlive()){
 					wt.interrupt();
 					wt = null;
 				}
 			}
 		});
+	}
+	
+	private void updateGoal(){
+		String text = goalTextField.getText();
+		try{
+			goal = Integer.parseInt(text);
+		}catch(Exception e){
+			goal = defaultGoal;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -105,7 +132,7 @@ public class TheCount extends JPanel{
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.pack();
 		for(int i = 0; i < 4; i++){
-			TheCount tc = new TheCount();
+			TheCount tc = new TheCount(i);
 			tc.setLayout(new BoxLayout(tc, BoxLayout.Y_AXIS));
 			SwingUtilities.invokeLater(new Runnable(){
 				public void run(){
