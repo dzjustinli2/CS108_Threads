4858cf9d7f315da18bb80e0f85c7b058f17cab8a
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-11 22:27:45 -0800

    Finished the preliminary Cracker, testing now

--- src/assign4/Cracker.java
+++ src/assign4/Cracker.java
@@ -1,6 +1,10 @@
 package assign4;
 
 import java.security.*;
+import java.util.concurrent.*;
+import java.util.*;
+import java.io.*;
+import java.lang.StringBuilder;
 
 public class Cracker {
 	// Array of chars used to produce strings
@@ -58,6 +62,8 @@ public class Cracker {
 		return hexToString(md.digest(word.getBytes()));
 	}
 	
+	/*-------------------------------*/
+	
 	private String generatedHash;
 	
 	//We're in generator mode
@@ -76,8 +82,108 @@ public class Cracker {
 		return generatedHash;
 	}
 	
+	/*-------------------------------*/
+	
+	private int numThreads;
+	private int passwordLength;
+	private byte[] hash;
+	private String password;
+	private CountDownLatch found;
+	private boolean foundPassword;
+	MessageDigest md;
+	
 	//We're in cracker mode
 	public Cracker(byte[] hash, int passwordLength, int numThreads){
+		this.hash = hash;
+		this.passwordLength = passwordLength;
+		this.numThreads = numThreads;
+		password = "";
+		found = new CountDownLatch(1);
+		foundPassword = false;
+		try{
+			md = MessageDigest.getInstance("SHA");
+		}catch(Exception e) {
+			System.out.println("Exception generating instance of message encryption algorithm");
+			e.printStackTrace();
+		}
+	}
+
+	private synchronized void setPassword(String password){
+		this.password = password;
+	}
+	
+	private synchronized void setFoundPassword(){
+		this.foundPassword = true;
+	}
+	
+	private synchronized boolean getFoundPassword(){
+		return this.foundPassword;
+	}
+	
+	public String findPassword(){
+		int vocabSize = CHARS.length;
+		int increment = vocabSize/numThreads;
+		int remainder = vocabSize%numThreads;
+		for(int i = 0; i < numThreads; i++){
+			Worker wk;
+			if(i == numThreads - 1){
+				wk = new Worker(i*increment,(i+1)*increment-1+remainder);
+			}else{
+				wk = new Worker(i*increment,(i+1)*increment-1);
+			}
+			wk.start();
+		}
+		try{
+			found.await();
+		}catch(InterruptedException e){
+			System.out.println("Main thread was interrupted before password was found");
+			e.printStackTrace();
+		}
+		return password;
+	}
+
+	public class Worker extends Thread{
+		//begin and end are inclusive
+		private int begin;
+		private int end;
+		
+		public Worker(int start, int finish){
+			super();
+			begin = start;
+			end = finish;
+		}
+		
+		public void run(){
+			try{
+				boolean val = false;
+				for(int i = begin; i <= end; i++){
+					Character ch = new Character(CHARS[i]);
+					val = makeString(ch.toString());
+					if(val) break;
+				}
+			}catch(Exception e){
+				System.out.println("Exception in thread run loop");
+				e.printStackTrace();
+			}
+		}
+		
+		private boolean makeString(String startingValue){
+			if(getFoundPassword()) return false;
+			if(startingValue.length() > passwordLength) return false;
+			byte[] myHash = md.digest(startingValue.getBytes());
+			if(myHash.equals(hash)){
+				setPassword(startingValue);
+				setFoundPassword();
+				found.countDown();
+				return true;
+			}
+			for(int i = 0; i < CHARS.length; i++){
+				Character ch = new Character(CHARS[i]);
+				String newValue = startingValue + ch.toString();
+				if(makeString(newValue)) return true;
+			}
+			return false;
+		}
 		
 	}
 	
@@ -91,7 +197,10 @@ public class Cracker {
 		String hash = args[0];
 		String length = args[1];
 		String threads = args[2];
-		ck = new Cracker(hash.getBytes(),Integer.parseInt(length),Integer.parseInt(threads));
+		ck = new Cracker(hexToArray(hash),Integer.parseInt(length),Integer.parseInt(threads));
+		String password = ck.findPassword();
+		System.out.println(password);
+		System.out.println("all done");
 	}
 
 }
--- src/assign4/CrackerTest.java
+++ src/assign4/CrackerTest.java
@@ -16,17 +16,28 @@ public class CrackerTest {
 		genMap.put("fm","adeb6f2a18fe33af368d91b09587b68e3abcb9a7");
 		genMap.put("a!","34800e15707fae815d7c90d49de44aca97e2d759");
 		genMap.put("xyz","66b27417d37e024c46526c2f6d358a754fc552f3");
+		genMap.put("molly","4181eecbd7a755d19fdf73887c54837cbecf63fd");
 	}
 
 	@Test
 	public void testGenerator() {
 		setMapUp();
 		Cracker ck = new Cracker("",false);
-		for(Map.Entry<String, String> accountEntry : genMap.entrySet()){
-			String hash = ck.generateHash(accountEntry.getKey());
-			assertEquals(hash,accountEntry.getValue());
+		for(Map.Entry<String, String> pwEntry : genMap.entrySet()){
+			String hash = ck.generateHash(pwEntry.getKey());
+			assertEquals(hash,pwEntry.getValue());
         }
-		
+	}
+	
+	@Test
+	public void testCrackerSmall() {
+		setMapUp();
+		for(Map.Entry<String, String> pwEntry : genMap.entrySet()){
+			String hash = pwEntry.getValue();
+			Cracker ck = new Cracker(hash.getBytes(),5,4);
+			String password = ck.findPassword();
+			assertEquals(password,pwEntry.getKey());
+        }
 	}
 
 }
